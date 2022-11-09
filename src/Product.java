731d0c5d5a1da4400e1e99c75f2d4d104850d4fc
mislavskii <jornjat@gmail.com>
2022-11-09 11:35:33 +0700

    minor change to Product

--- src/Product.java
+++ src/Product.java
@@ -35,10 +35,6 @@ public class Product {
 
     @Override
     public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", price=" + price +
-                ", count=" + count +
-                '}';
+        return "Product{" + "name='" + name + '\'' + ", price=" + price + ", count=" + count + '}';
     }
 }
